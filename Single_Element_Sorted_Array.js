d4d79fb1acbab2794dbcd99c25445789e108267e
Bikas <b.kgupta83560@gmail.com>
2026-09-01 12:13:38 +0530

    Added More Test Case

--- Single_Element_Sorted_Array.js
+++ Single_Element_Sorted_Array.js
@@ -31,34 +31,3 @@ console.log(singleNonDuplicate([1,1,2,2,3,3,4])); // 4
 console.log(singleNonDuplicate([1,1,2,2,3,3,4,4,5])); // 5
 console.log(singleNonDuplicate([-5,-5,-2,-2,0,1,1])); // 0
 
-
-
-
-
-
-
-
-
-
-//     let low = 0;
-//     let high = nums.length - 1;
-
-//     while (low < high) {
-//         let mid = Math.floor((low + high) / 2);
-
-//         // Make mid even
-//         if (mid % 2 === 1) {
-//             mid--;
-//         }
-
-//         if (nums[mid] === nums[mid + 1]) {
-//             // Pair is correct, single is on the right
-//             low = mid + 2;
-//         } else {
-//             // Pair is broken, single is on the left
-//             high = mid;
-//         }
-//     }
-
-//     return nums[low];
-// };
